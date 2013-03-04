334325b00e488e2e8e36729c59e1836b6c7fe465
bumbu <bmbalex@gmail.com>
2013-03-05 00:22:10 +0200

    :sunrise: Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FAFLabs::Application.config.secret_key_base = '05fbdb6cafcc586cdbcbad947cd391ac6a7107cd9f11ba6a15f6227befa0f4a6be08891eec7f25bb51d1dbb90133466e38ecd5b33161e9a598484c13eb7a82c7'

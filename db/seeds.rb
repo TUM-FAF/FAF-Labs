f8f80498c431297c30bc337880ab80c8c808e155
Ivan Zarea <zarea.ion@gmail.com>
2013-03-21 22:26:16 +0200

    :seedling: Added some database seeds so we can have initial data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Course.create [{ name: 'Numerical Analysis' },
+               { name: 'Windows Programming' },
+               { name: 'Interactive Development Environments' }]

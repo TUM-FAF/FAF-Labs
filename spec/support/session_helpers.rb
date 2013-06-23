9f5fdd434da8ce6c1b0daf2d8b9913529da4e89c
Ivan Zarea <zarea.ion@gmail.com>
2013-06-23 16:52:52 +0300

    :two_hearts: We don't need the sign_in spec

--- spec/support/session_helpers.rb
+++ spec/support/session_helpers.rb
@@ -1,13 +1,5 @@
 module Features
   module SessionHelpers
-    def sign_up_with(user)
-      visit new_user_registration_path
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-      fill_in 'Password confirmation', with: user.password
-      click_button 'Sign up'
-    end
-
     def sign_in_with(user)
       visit new_user_session_path
       fill_in 'Email', with: user.email

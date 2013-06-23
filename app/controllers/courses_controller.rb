4d1dff8322f2dd613ffdc860565d03d382ad71d5
Ivan Zarea <zarea.ion@gmail.com>
2013-06-23 18:06:55 +0300

    :poop: We can create courses now

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,4 +4,13 @@ class CoursesController < ApplicationController
     @course = Course.new
   end
 
+  def create
+    Course.create!(course_params)
+    redirect_to dashboard_path
+  end
+
+  private
+    def course_params
+      params.require(:course).permit!
+    end
 end

83aca1962bb6e6e9f2effc6aed3d46d1d58f50eb
Ivan Zarea <zarea.ion@gmail.com>
2013-06-23 18:31:20 +0300

    :scroll: Added a course factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -3,4 +3,10 @@ FactoryGirl.define do
     email     'student@faf.md'
     password  'gagarin-mujik'
   end
+
+  factory :course do
+    name      "Software testing"
+    summary   "A course about TDD"
+    syllabus  "In this course we will see how to create factories. All the pros and cons"
+  end
 end

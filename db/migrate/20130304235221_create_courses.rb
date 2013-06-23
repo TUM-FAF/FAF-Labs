74c8138500b5687c93a6c0098bfc00d89123d088
Ivan Zarea <zarea.ion@gmail.com>
2013-06-23 18:08:04 +0300

    :loop: Added some fields to the course

--- db/migrate/20130304235221_create_courses.rb
+++ db/migrate/20130304235221_create_courses.rb
@@ -2,6 +2,8 @@ class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
       t.string :name
+      t.text :summary
+      t.text :syllabus
 
       t.timestamps
     end

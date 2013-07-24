078d85c07419188bcc782218ecff8222a674ea9d
Requiem- <e.ungureanu92@gmail.com>
2013-07-24 16:15:56 +0300

    ::ocean: Added feature about creating an assignment

---
+++ spec/features/assignment_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'Lecturer creates an assignment' do
+	let(:user)    { FactoryGirl.create(:user) }
+  let!(:course) { FactoryGirl.create(:course) }	
+  # let(:subgroup){ FactoryGirl.create(:subgroup) }
+
+	scenario 'the lecturer has a course and subgroup' do
+		sign_in_with user
+  	click_link course.name
+  	click_link 'Create subgroup'
+    current_path.should == new_course_subgroup_path(course)
+    fill_in 'Name', with: 'Subgroup I'
+    click_button 'Save Subgroup'
+    click_link 'Subgroup I'
+    current_path.should == course_subgroup_path(course)
+    click_link 'New assignment'
+    current_path.should == new_subgroup_assignment(course.subgroups)
+    fill_in 'name', with: 'assignment one'
+    click_link 'Create assignment'
+    current_path.should == course_subgroup_path(course)
+    page.should have.content == 'assignment one'
+  end
+end

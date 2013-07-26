fae535162394dc7423b596352cfd5caef064cab7
Requiem- <e.ungureanu92@gmail.com>
2013-07-26 13:01:41 +0300

    :octopus: Added edit assignment scenario

--- spec/features/assignment_spec.rb
+++ spec/features/assignment_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
-feature 'Lecturer creates an assignment' do
+feature 'Lecturer has a course with a subgroup' do
 	let(:user)    { FactoryGirl.create(:user) }
   let!(:course) { FactoryGirl.create(:course) }	
   
-
-	scenario 'the lecturer has a course and subgroup' do
+	scenario 'lecturer creates an assignment' do
 		sign_in_with user
   	click_link course.name
   	click_link 'Create subgroup'
@@ -20,5 +19,25 @@ feature 'Lecturer creates an assignment' do
     click_button 'Create assignment'
     current_path.should == subgroup_path(course)
     page.should have_content('assignment one')
+  end
+
+  scenario 'lecturer edits an assignment' do
+    sign_in_with user
+    click_link course.name
+    click_link 'Create subgroup'
+    fill_in 'Name', with: 'Subgroup I'
+    click_button 'Save Subgroup'
+    click_link 'Subgroup I'
+    click_link 'New assignment'
+    fill_in 'Name', with: 'assignment one'
+    click_button 'Create assignment'
+    page.should have_content('Edit')
+    click_link 'Edit'
+    page.should have_content('Name')
+    page.should have_content('Save')
+    fill_in 'Name', with: '(Edit)'
+    click_button 'Save'
+    current_path.should == subgroup_path(course)
+    page.should have_content('assignment one(Edit)')    
   end
 end
